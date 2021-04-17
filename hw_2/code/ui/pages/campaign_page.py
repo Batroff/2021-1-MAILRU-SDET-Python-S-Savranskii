c3e25461c51631e0c97296a0ef40866aa62f0f1f
Sergey Savransky <savran46@mail.ru>
2021-04-17 18:21:04 +0300

    move code from TestCampaign.test_create to create_campaign method, remove comment

--- hw_2/code/ui/pages/campaign_page.py
+++ hw_2/code/ui/pages/campaign_page.py
@@ -1,3 +1,5 @@
+import os
+
 import allure
 
 from ui.locators.pages_locators import CampaignPageLocators
@@ -7,6 +9,21 @@ from ui.pages.base_page import BasePage
 class CampaignPage(BasePage):
     locators = CampaignPageLocators()
     url = 'https://target.my.com/campaign/new'
+
+    def create_campaign(self, repo_root, time_now):
+        self.choose_objective('Трафик')
+        self.input_objective_link('mail.ru')
+
+        campaign_name = f'test_campaign_{time_now}'
+        self.input_campaign_name(campaign_name)
+
+        self.choose_format('Баннер')
+        file_path = os.path.join(repo_root, 'ui', 'banner.png')
+        self.upload_banner(file_path)
+
+        self.click(self.locators.CREATE_BTN)
+
+        return campaign_name
 
     @allure.step('Choose objective')
     def choose_objective(self, name):
@@ -25,9 +42,6 @@ class CampaignPage(BasePage):
     @allure.step('Choose campaign format')
     def choose_format(self, name):
         self.click(self.format_locator(self.locators.ADS_FORMAT_TEMPLATE, name))
-        # self.click((self.locators.ADS_FORMAT_TEMPLATE[0],
-        #             self.locators.ADS_FORMAT_TEMPLATE[1].format(name)
-        #             ))
 
     def upload_banner(self, filepath):
         self.upload_file(locator=self.locators.ADS_FORMAT_INPUT, file=filepath)
