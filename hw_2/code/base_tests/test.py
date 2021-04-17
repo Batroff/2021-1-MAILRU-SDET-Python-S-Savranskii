41c3e60fc7fa72723f6bc80c5273df79d86de254
Sergey Savransky <savran46@mail.ru>
2021-04-17 18:26:37 +0300

    remove unused imports; rework test structure

--- hw_2/code/base_tests/test.py
+++ hw_2/code/base_tests/test.py
@@ -1,4 +1,3 @@
-import os
 import allure
 import pytest
 
@@ -14,17 +13,15 @@ class TestWrongLogin(BaseCase):
     def test_format_negative(self):
         self.base_page.login(login='testing_data', password='some_pas')
 
-        wrong_data_error = self.base_page.find(self.base_page.locators.FORM_WRONG_DATA_FORMAT)
-        wrong_data_error_text = self.base_page.wait_text_changed(wrong_data_error)
-        assert 'Введите email или телефон' == wrong_data_error_text
+        assert self.base_page.find_wrong_format_error() is True
 
     @pytest.mark.UI
     @allure.description('Test: wrong account data')
     def test_account_negative(self):
         self.base_page.login(login='wrong_email@mail.ru', password='password')
-        login_page = self.get_login_page()
+        login_page = self.go_to_login_page()
 
-        assert login_page.find_error().text == 'Error'
+        assert login_page.find_error() is True
 
 
 class TestCampaign(BaseCase):
@@ -32,23 +29,13 @@ class TestCampaign(BaseCase):
     @pytest.mark.UI
     @allure.description('Test: create new campaign and check exist of it')
     def test_create(self, repo_root, time_now):
-        campaign_page = self.dashboard_page.create_campaign()
-        campaign_page.choose_objective('Трафик')
-        campaign_page.input_objective_link('mail.ru')
+        campaign_page = self.dashboard_page.go_to_campaign_page()
+        campaign_name = campaign_page.create_campaign(repo_root, time_now)
 
-        campaign_name = f'test_campaign_{time_now}'
-        campaign_page.input_campaign_name(campaign_name)
-
-        campaign_page.choose_format('Баннер')
-        file_path = os.path.join(repo_root, 'ui', 'banner.png')
-        campaign_page.upload_banner(file_path)
-
-        campaign_page.click(campaign_page.locators.CREATE_BTN)
         assert self.dashboard_page.is_opened(self.dashboard_page._url_contains)
         assert self.dashboard_page.campaign_exists(campaign_name)
 
-        self.dashboard_page.select_campaign(campaign_name)
-        self.dashboard_page.remove_campaign()
+        self.dashboard_page.remove_campaign(campaign_name)
         self.dashboard_page.refresh_page()
 
 
@@ -57,25 +44,17 @@ class TestSegments(BaseCase):
     @pytest.mark.UI
     @allure.description('Test: create new segment and check existence of it')
     def test_create(self, time_now):
-        segment_page = self.dashboard_page.go_to_segments()
+        segment_page = self.dashboard_page.go_to_segment_page()
 
-        source_id = segment_page.create_group_source(link='https://vk.com/overhear_mtu')
-
-        segment_name = f'test_create_{time_now}'
-        segment_page.go_to_segment_list()
-        segment_page.create_segment(name=segment_name, source_id=source_id)
-        assert segment_page.segment_exists(name=segment_name) == "Exists"
+        segment_name = segment_page.create_segment(time_now, name_prefix='test_create')
+        assert segment_page.segment_exists(segment_name) == "Exists"
 
     @pytest.mark.UI
     @allure.description('Test: remove created segment')
     def test_remove(self, time_now):
-        segment_page = self.dashboard_page.go_to_segments()
+        segment_page = self.dashboard_page.go_to_segment_page()
 
-        source_id = segment_page.create_group_source(link='https://vk.com/overhear_mtu')
-
-        segment_name = f'test_remove_{time_now}'
-        segment_page.go_to_segment_list()
-        segment_page.create_segment(name=segment_name, source_id=source_id)
+        segment_name = segment_page.create_segment(time_now, name_prefix='test_remove')
 
         segment_page.remove_segment(name=segment_name)
         segment_page.refresh_page()
