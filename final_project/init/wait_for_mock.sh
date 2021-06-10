c2421fa0df4ae55c00f794f56ce7df12a949b1bb
batroff <savran46@mail.ru>
2021-06-10 14:34:19 +0300

    - add requirements.txt for final_project - remove unused code from wait_for

--- final_project/init/wait_for_mock.sh
+++ final_project/init/wait_for_mock.sh
@@ -6,10 +6,3 @@ while [[ "$(curl -o /dev/null -s -w "%{http_code}\n" http://vk_mock:8090/is_aliv
 done;
 
 echo mock is up;
-
-#while [[ "$(curl -o /dev/null -s -w "%{http_code}\n" http://selenoid:4444/status)" != "200" ]]; do
-#  echo waiting for selenoid...;
-#  sleep 3;
-#done;
-#
-#echo selenoid is up;
